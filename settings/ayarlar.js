67a4c5595b862cb8b3da3d00d33a5c7de2b6e80c
Juadiis <wiki.neseos@gmail.com>
2024-08-10 10:26:31 -0700

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 module.exports = {
     TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHIP || "SAHİP İD", //sahip id :()
 }
